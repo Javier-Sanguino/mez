3be3a763fa2d4e1058c15b51f98d0b29f865dcf8
Javier-Sanguino <fjse0505@gmail.com>
2021-10-20 22:33:47 -0500

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const btnShow = document.getElementById('btn-show');
+const secondaryServices = document.getElementById('secondaryServices');
+
+function showServices(){
+    secondaryServices.classList.toggle('active');
+}
