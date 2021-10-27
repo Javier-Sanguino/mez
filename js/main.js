f44356113f74d6bfe881895c68eb4c650ffeb262
Javier-Sanguino <fjse0505@gmail.com>
2021-10-27 09:42:33 -0500

    Mejoras al diseño

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const btnShow = document.getElementById('btn-show');
 const secondaryServices = document.getElementById('secondaryServices');
+const btnMenu = document.getElementById('iconMenu');
+const menu = document.getElementById('menu');
 
 function showServices(){
     secondaryServices.classList.toggle('active');
+}
+
+function showMenu() {
+    menu.classList.toggle('active');
 }
