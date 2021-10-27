de2b607732b5e2dcf5cfd7fac789ab7fa355d0d2
Javier-Sanguino <fjse0505@gmail.com>
2021-10-27 11:46:36 -0500

    Terminacion de estilos del menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 const btnShow = document.getElementById('btn-show');
 const secondaryServices = document.getElementById('secondaryServices');
 const btnMenu = document.getElementById('iconMenu');
-const menu = document.getElementById('menu');
-
+const menu = document.getElementById('menu-mobile');
+const btnCloseMenu = document.getElementById('close-icon');
 function showServices(){
     secondaryServices.classList.toggle('active');
 }
 
 function showMenu() {
     menu.classList.toggle('active');
+    btnMenu.classList.toggle('hidden')
 }
